a5584cb73fa942e1462e25dace45ae2648f9030b
Kirill Zaitsev <k.zaitsev@me.com>
2012-08-29 18:55:51 +0400

    fix to be  used with our celery

--- recommends/tasks.py
+++ recommends/tasks.py
@@ -1,25 +1,23 @@
-from celery.task import task, periodic_task
+from celery import task
 from celery.schedules import crontab
 from .utils import filelock
 
 from .settings import RECOMMENDS_TASK_RUN, RECOMMENDS_TASK_CRONTAB
 
 
-if RECOMMENDS_TASK_RUN:
+@task(name='recommends_precompute')
+def recommends_precompute():
+    from .providers import recommendation_registry
 
-    @periodic_task(run_every=crontab(**RECOMMENDS_TASK_CRONTAB))
-    def recommends_precompute():
-        from .providers import recommendation_registry
+    # I know this is weird, but it's faster (tested on CPyhton 2.6.5)
+    def _precompute(provider_instance):
+        provider_instance.precompute()
 
-        # I know this is weird, but it's faster (tested on CPyhton 2.6.5)
-        def _precompute(provider_instance):
-            provider_instance.precompute()
-
-        with filelock('recommends_precompute.lock'):
-            [_precompute(provider_instance) for provider_instance in recommendation_registry.get_vote_providers()]
+    with filelock('recommends_precompute.lock'):
+        [_precompute(provider_instance) for provider_instance in recommendation_registry.get_vote_providers()]
 
 
-@task
+@task(name='remove_suggestions')
 def remove_suggestions(rated_model, object_id):
     from django.db.models import get_model
     from recommends.providers import recommendation_registry
@@ -31,7 +29,7 @@ def remove_suggestions(rated_model, object_id):
     provider_instance.storage.remove_recommendations(obj)
 
 
-@task
+@task(name='remove_similarities')
 def remove_similarities(rated_model, object_id):
     from django.db.models import get_model
     from recommends.providers import recommendation_registry
